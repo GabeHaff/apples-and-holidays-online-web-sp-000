6594f9502f17254aa0949791cb5cf7a5499f33ba
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-03-13 15:10:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,14 @@ def all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+holiday_name = []
+   holiday_hash.values.each do |info| 
+     info.each do |holiday, supplies|
+       if supplies.include?("BBQ")
+         holiday_name << holiday 
+       end 
+     end 
+   end 
 end
 
 
